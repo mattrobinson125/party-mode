6106e019b3ad184ca3239963114e2b4e2c135ed2
mattrobinson125 <mattrobinson125@gmail.com>
2016-10-02 01:39:22 -0400

    More front end styling, also added a GET from spotify to retrieve profile info.

--- client/app/partyCreate/partyCreate.component.js
+++ client/app/partyCreate/partyCreate.component.js
@@ -1,15 +1,30 @@
 'use strict';
 const angular = require('angular');
 const ngRoute = require('angular-route');
+const ngCookies = require('angular-cookies');
 
 
 import routes from './partyCreate.routes';
 
 export class PartyCreateComponent {
   /*@ngInject*/
-  constructor() {
-    this.formData = {}
+  constructor($http, $cookies) {
+    this.$http = $http;
+    this.$cookies = $cookies;
+    this.access_token = $cookies.get('access_token');
   }
+
+  $onInit() {
+    var options = {
+      headers: { 'Authorization': 'Bearer ' + this.access_token },
+      json: true
+    };
+    this.$http.get('https://api.spotify.com/v1/me', options)
+      .then(response => {
+        this.host = response.data.display_name;
+      });
+  }
+
   processForm(){
     var form = this.formData;
     if(!form.name || !form.visibility || !form.zip){
@@ -22,11 +37,11 @@ export class PartyCreateComponent {
   }
 }
 
-export default angular.module('partyModeApp.partyCreate', [ngRoute])
+export default angular.module('partyModeApp.partyCreate', [ngRoute, ngCookies])
   .config(routes)
   .component('partyCreate', {
     template: require('./partyCreate.html'),
-    controller: PartyCreateComponent,
+    controller: ['$http', '$cookies', PartyCreateComponent],
     controllerAs: 'partyCreateCtrl'
   })
   .name;
